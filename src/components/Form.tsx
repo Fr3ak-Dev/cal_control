dc0b8d541a5dfef9f7f816fcbd283d2eaac61592
gustavo <kg.2k.pl@gmail.com>
2025-04-11 20:26:55 -0400

    add activity useState

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
 import { categories } from "../data/categories"
 
 export default function Form() {
+
+    const [activity, setActivity] = useState({
+        category: 1,
+        name: '',
+        calories: 0
+    })
+
     return (
         <form className="space-y-5 bg-white shadow p-10 rounded-lg">
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="category" className="font-bold">Categoría</label>
-                <select id="category" className="border border-slate-300 p-2 rounded-lg w-full bg-white cursor-pointer">
+                <select id="category" className="border border-slate-300 p-2 rounded-lg w-full bg-white cursor-pointer"
+                    value={activity.category}>
                     {categories.map(category => (
                         <option key={category.id} value={category.id}>
                             {category.name}
@@ -14,16 +23,18 @@ export default function Form() {
                 </select>
             </div>
             <div className="grid grid-cols-1 gap-3">
-                <label htmlFor="activity" className="font-bold">Actividad</label>
-                <input type="text" id="activity"
+                <label htmlFor="name" className="font-bold">Actividad</label>
+                <input type="text" id="name"
                     className="border border-slate-300 p-2 rounded-lg"
-                    placeholder="Ej: Comida, Jugo de Naranja, Ensalada, Pesas, Bicicleta" />
+                    placeholder="Ej: Comida, Jugo de Naranja, Ensalada, Pesas, Bicicleta"
+                    value={activity.name} />
             </div>
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="calories" className="font-bold">Calorias</label>
                 <input type="number" id="calories"
                     className="border border-slate-300 p-2 rounded-lg"
-                    placeholder="Calorías. Ej: 300 o 500" />
+                    placeholder="Calorías. Ej: 300 o 500"
+                    value={activity.calories} />
             </div>
             <input type="submit"
                 className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer rounded-lg" />
