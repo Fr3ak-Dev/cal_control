83f98f4feba9e4ebe046b7e25dc4271eb5bd7c3b
gustavo <kg.2k.pl@gmail.com>
2025-04-11 20:41:45 -0400

    add handleChange function to manage form input state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,12 +9,20 @@ export default function Form() {
         calories: 0
     })
 
+    const handleChange = (e) => {
+        setActivity({
+            ...activity,
+            [e.target.id]: e.target.value
+        })
+    }
+
     return (
         <form className="space-y-5 bg-white shadow p-10 rounded-lg">
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="category" className="font-bold">Categoría</label>
                 <select id="category" className="border border-slate-300 p-2 rounded-lg w-full bg-white cursor-pointer"
-                    value={activity.category}>
+                    value={activity.category}
+                    onChange={handleChange}>
                     {categories.map(category => (
                         <option key={category.id} value={category.id}>
                             {category.name}
@@ -27,14 +35,16 @@ export default function Form() {
                 <input type="text" id="name"
                     className="border border-slate-300 p-2 rounded-lg"
                     placeholder="Ej: Comida, Jugo de Naranja, Ensalada, Pesas, Bicicleta"
-                    value={activity.name} />
+                    value={activity.name}
+                    onChange={handleChange} />
             </div>
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="calories" className="font-bold">Calorias</label>
                 <input type="number" id="calories"
                     className="border border-slate-300 p-2 rounded-lg"
                     placeholder="Calorías. Ej: 300 o 500"
-                    value={activity.calories} />
+                    value={activity.calories}
+                    onChange={handleChange} />
             </div>
             <input type="submit"
                 className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer rounded-lg" />
