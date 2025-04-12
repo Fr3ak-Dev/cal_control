05ea1f5dd713c08590ebceb544e44307da91d265
gustavo <kg.2k.pl@gmail.com>
2025-04-12 19:30:09 -0400

    define actions in the reducer

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,7 +1,12 @@
 import { Activity } from "../types"
 
 export type ActivityActions = {
-
+    /**
+     * Las acciones tiene un tipo y un payload
+     * type: es la descripción de la acción
+     * payload: es el objeto (información) que modifica o agrega al state
+     */
+    type: 'save-activity', payload: { newActivity: Activity }
 }
 
 // Define the state type
@@ -18,5 +23,7 @@ export const activityReducer = (
     state: ActivityState = initialState, // Default state
     action: ActivityActions // Action to process
 ) => {
-    
+    if (action.type === 'save-activity') {
+        console.log('Desde el type save-activity')
+    }
 }
