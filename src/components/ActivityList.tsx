29de4f244e06a856995509a9c83ac62b105b7059
gustavo <kg.2k.pl@gmail.com>
2025-04-12 23:19:37 -0400

    Add @heroicons/react dependency and integrate PencilSquareIcon in ActivityList component

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react"
 import { Activity } from "../types"
 import { categories } from "../data/categories"
+import { PencilSquareIcon } from "@heroicons/react/24/outline"
 
 type ActivityListProps = {
     activities: Activity[]
@@ -32,8 +33,12 @@ export default function ActivityList({ activities }: ActivityListProps) {
                             <span>Calorías</span>
                         </p>
                     </div>
-                    <div>
-                        Y
+                    <div className="flex gap-5 items-center">
+                        <button>
+                            <PencilSquareIcon 
+                                className="h-8 w-8 text-gray-800"
+                            />
+                        </button>
                     </div>
                 </div>
             ))}
