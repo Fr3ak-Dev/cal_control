ae2ee2bd3b2e8fa0bcabeddf4acaae8be09ebb0f
gustavo <kg.2k.pl@gmail.com>
2025-04-12 23:09:11 -0400

    Refactor ActivityList component to use useMemo for category names

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,4 +1,6 @@
+import { useMemo } from "react"
 import { Activity } from "../types"
+import { categories } from "../data/categories"
 
 type ActivityListProps = {
     activities: Activity[]
@@ -6,17 +8,23 @@ type ActivityListProps = {
 
 export default function ActivityList({ activities }: ActivityListProps) {
 
+    const categoryName = useMemo(() =>
+        (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''),
+        [activities]
+    )
+
     return (
         <>
             <h2 className="text-4xl font-bold text-slate-600 text-center">
                 Comida y Actividades
             </h2>
 
-            {activities.map( activity => (
+            {activities.map(activity => (
                 <div key={activity.id} className="bg-white px-5 py-10 mt-5 flex justify-between">
                     <div className="space-y-2 relative">
-                        <p>
-                            {activity.category}
+                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
+                                ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                            {categoryName(+activity.category)}
                         </p>
                         <p className="text-2xl font-bold pt-5">{activity.name}</p>
                         <p className="font-black text-4xl text-lime-500">
