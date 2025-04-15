578e37c1fc4c3e278fed7a5e63c4572d5806ae4d
gustavo <kg.2k.pl@gmail.com>
2025-04-15 00:58:06 -0400

    Add CalorieTracker component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useReducer } from "react"
 import Form from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
 import ActivityList from "./components/ActivityList"
+import CalorieTracker from "./components/CalorieTracker"
 
 function App() {
 
@@ -20,7 +21,7 @@ function App() {
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de Calorías
           </h1>
-          <button className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm"
+          <button className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
             disabled={!canResetApp()}
             onClick={() => dispatch({ type: 'reset-activity' })}>
             Reiniciar App
@@ -33,6 +34,14 @@ function App() {
           <Form
             dispatch={dispatch}
             state={state}
+          />
+        </div>
+      </section>
+
+      <section className="bg-gray-800 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker 
+            activities={state.activities}
           />
         </div>
       </section>
